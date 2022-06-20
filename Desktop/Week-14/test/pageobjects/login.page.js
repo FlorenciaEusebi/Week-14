2de18a122f25d9b9b5042315a70ce7e482854a8e
Florencia Eusebi <eusebiflorencia@gmail.com>
2022-06-20 17:56:38 +0200

    BaSP-W14: login page automated tests completed

--- Desktop/Week-14/test/pageobjects/login.page.js
+++ Desktop/Week-14/test/pageobjects/login.page.js
@@ -1,24 +1,31 @@
 class loginPage {
-  //Getters
-  get inputUsername() { return $('#user-name')}
-  get inputPassword() { return $('#password')}
-  get btnLogin() { return $('#login-button')}
-  get errorContainer(){ return $('.error-message-container.error')}
-  get botImg(){ return $('.bot_column')}
-  get loginLogo(){ return $('.login_logo')}
-  //Setters
-  async setUsername(username) {
-    await this.inputUsername.setValue(username);
-  }
-  async setPassword(password) {
-    await this.inputPassword.setValue(password);
-  }
-  //Methods
-  async login(username, password) {
-    await this.setUsername(username);
-    await this.setPassword(password);
-    await this.btnLogin.click();
-  }
-}
 
+    //Getters
+    get inputUsername() { return $('#user-name')}
+    get inputPassword() { return $('#password')}
+    get btnLogin() { return $('#login-button')}
+    get errorContainer(){ return $('.error-message-container.error')}
+    get botImg(){ return $('.bot_column')}
+    get loginLogo(){ return $('.login_logo')}
+
+    //Setters
+    async setUsername(username) {
+      await this.inputUsername.setValue(username);
+    }
+
+    async setPassword(password) {
+      await this.inputPassword.setValue(password);
+    }
+
+    //Methods
+    async login(username, password) {
+      await this.setUsername(username);
+      await this.setPassword(password);
+      await this.btnLogin.click();
+    }
+ 
+    open () {
+      return browser.url('https://www.saucedemo.com/');
+    }
+}
 module.exports = new loginPage()
--- Desktop/Week-14/test/specs/login.spec.js
+++ Desktop/Week-14/test/specs/login.spec.js
@@ -4,31 +4,82 @@ describe ('Login page testing', () => {
 
   beforeAll('Open browser', () => {
     browser.url('https://www.saucedemo.com/')
-  })
+  });
+
   describe('Username input test', () => {
+
     it('Empty username should display an error', async () => {
       await LoginPage.login('','secret_sauce');
       await expect(LoginPage.errorContainer).toHaveText('Epic sadface: Username is required');
-    })
+    });
+
     it('Invalid username', async () => {
       await LoginPage.login('invalid','secret_sauce');
       await expect(LoginPage.errorContainer).toHaveText('Epic sadface: Username and password do not match any user in this service');
-    })
-  })
+    });
+  });
+
+  describe ('Login with locked out user', () => {
+
+    it('Using locked out user to log in should display an error', async () => {
+      await LoginPage.open();
+      await LoginPage.login('locked_out_user', 'secret_sauce');
+      await expect(LoginPage.errorContainer).toHaveText('Epic sadface: Sorry, this user has been locked out.');
+    });
+  });
+
+  describe ('Login with problem user', () => {
+
+    it('Using problem user to log in', async () => {
+      await LoginPage.open();
+      await LoginPage.login('problem_user', 'secret_sauce');
+      await expect(browser).toHaveUrl('https://www.saucedemo.com/inventory.html');
+    });
+
+    it('Checking images content when accessing with problem user', async () => {
+      const imgSrc = await $('#item_4_img_link > img').getAttribute('src');
+      await expect(imgSrc).toBe('/static/media/sl-404.168b1cce.jpg');
+    });
+  });
+
+  describe ('Login with performance glitch user', () => {
+
+    it ('Using performance glitch user to log in', async () => {
+      await LoginPage.open();
+      await LoginPage.login('performance_glitch_user', 'secret_sauce');
+      await browser.setTimeout({'page load': 10000 });
+      await expect(browser).toHaveUrl('https://www.saucedemo.com/inventory.html');
+    });
+  });
+
+  describe('Login with valid user', () => {
+
+    it('Login success', async () => {
+      await LoginPage.open();
+      await LoginPage.login('standard_user','secret_sauce');
+      await expect(browser).toHaveUrl('https://www.saucedemo.com/inventory.html')
+    });
+  });
+
   describe('Elements to be displayed', () => {
+
     it('Verify botImg to be displayed', async () =>{
       await expect(LoginPage.botImg).toBeDisplayed();
-    })
+    });
+
     it ('Verify login logo', async () => {
       await expect(LoginPage.loginLogo).toBeDisplayed();
-    })
-  })
+    });
+  });
+
   describe('Checking log in content with success login', () => {
+
     it('Img content', async () => {
-    await LoginPage.login('standard_user','secret_sauce');
-    const imgSrc = await $('#item_4_img_link > img').getAttribute('src');
-    await expect(imgSrc).toBe('/static/media/sauce-backpack-1200x1500.34e7aa42.jpg')
-    })
-  })
+      await LoginPage.login('standard_user','secret_sauce');
+      const imgSrc = await $('#item_4_img_link > img').getAttribute('src');
+      await expect(imgSrc).toBe('/static/media/sauce-backpack-1200x1500.34e7aa42.jpg')
+    });
+  });
+
 
 })
