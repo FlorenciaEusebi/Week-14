0d27edea9f3f258eb2211b13e95069b5f067989a
Florencia Eusebi <eusebiflorencia@gmail.com>
2022-06-25 10:30:49 +0200

    BaSP-2022: uncommented code to show passed tests

--- Desktop/Week-14/test/specs/inventory.spec.js
+++ Desktop/Week-14/test/specs/inventory.spec.js
@@ -1,173 +1,173 @@
-// const InventoryPage = require('../pageobjects/inventory.page');
-// const LoginPage = require('../pageobjects/login.page');
+const InventoryPage = require('../pageobjects/inventory.page');
+const LoginPage = require('../pageobjects/login.page');
 
-// describe ('Inventory page testing', () => {
+describe ('Inventory page testing', () => {
 
-//   beforeAll('Open browser', () => {
-//     browser.url('https://www.saucedemo.com/')
-//   });
+  beforeAll('Open browser', () => {
+    browser.url('https://www.saucedemo.com/')
+  });
 
-//   describe('Login with valid user', () => {
+  describe('Login with valid user', () => {
 
-//     it('Login success', async () => {
-//       await LoginPage.open();
-//       await LoginPage.login('standard_user','secret_sauce');
-//       await expect(browser).toHaveUrl('https://www.saucedemo.com/inventory.html')
-//     });
-//   });
+    it('Login success', async () => {
+      await LoginPage.open();
+      await LoginPage.login('standard_user','secret_sauce');
+      await expect(browser).toHaveUrl('https://www.saucedemo.com/inventory.html')
+    });
+  });
 
-//   describe ('Backpack inventory test', () => {
+  describe ('Backpack inventory test', () => {
 
-//     it('Checking Img content', async () => {
-//       const imgSrc = await $('#item_4_img_link > img').getAttribute('src');
-//       await expect(imgSrc).toBe('/static/media/sauce-backpack-1200x1500.34e7aa42.jpg')
-//     });
+    it('Checking Img content', async () => {
+      const imgSrc = await $('#item_4_img_link > img').getAttribute('src');
+      await expect(imgSrc).toBe('/static/media/sauce-backpack-1200x1500.34e7aa42.jpg')
+    });
 
-//     it('Checking Img title link', async () => {
-//       const titleLink = await $('#item_4_title_link');
-//       await expect(titleLink).toHaveHrefContaining('#')
-//     });
+    it('Checking Img title link', async () => {
+      const titleLink = await $('#item_4_title_link');
+      await expect(titleLink).toHaveHrefContaining('#')
+    });
 
-//     it('Checking add button is clickable', async () => {
-//       const addBtn = await $('#add-to-cart-sauce-labs-backpack');
-//       await expect (addBtn).toBeClickable();
-//     });
-//   });
+    it('Checking add button is clickable', async () => {
+      const addBtn = await $('#add-to-cart-sauce-labs-backpack');
+      await expect (addBtn).toBeClickable();
+    });
+  });
 
-//   describe ('Bike light inventory test', () => {
+  describe ('Bike light inventory test', () => {
 
-//     it('Checking Img content', async () => {
-//       const imgSrc = await $('#item_0_img_link > img').getAttribute('src');
-//       await expect(imgSrc).toBe('/static/media/bike-light-1200x1500.a0c9caae.jpg')
-//     });
+    it('Checking Img content', async () => {
+      const imgSrc = await $('#item_0_img_link > img').getAttribute('src');
+      await expect(imgSrc).toBe('/static/media/bike-light-1200x1500.a0c9caae.jpg')
+    });
 
-//     it('Checking Img title link', async () => {
-//       const titleLink = await $('#item_0_title_link');
-//       await expect(titleLink).toHaveHrefContaining('#')
-//     });
+    it('Checking Img title link', async () => {
+      const titleLink = await $('#item_0_title_link');
+      await expect(titleLink).toHaveHrefContaining('#')
+    });
 
-//     it('Checking add button is clickable', async () => {
-//       const addBtn = await $('#add-to-cart-sauce-labs-bike-light');
-//       await expect (addBtn).toBeClickable();
-//     });
-//   });
+    it('Checking add button is clickable', async () => {
+      const addBtn = await $('#add-to-cart-sauce-labs-bike-light');
+      await expect (addBtn).toBeClickable();
+    });
+  });
 
-//   describe ('T-Shirt inventory test', () => {
+  describe ('T-Shirt inventory test', () => {
 
-//     it('Checking Img content', async () => {
-//       const imgSrc = await $('#item_1_img_link > img').getAttribute('src');
-//       await expect(imgSrc).toBe('/static/media/bolt-shirt-1200x1500.c0dae290.jpg')
-//     });
+    it('Checking Img content', async () => {
+      const imgSrc = await $('#item_1_img_link > img').getAttribute('src');
+      await expect(imgSrc).toBe('/static/media/bolt-shirt-1200x1500.c0dae290.jpg')
+    });
 
-//     it('Checking Img title link', async () => {
-//       const titleLink = await $('#item_1_title_link');
-//       await expect(titleLink).toHaveHrefContaining('#')
-//     });
+    it('Checking Img title link', async () => {
+      const titleLink = await $('#item_1_title_link');
+      await expect(titleLink).toHaveHrefContaining('#')
+    });
 
-//     it('Checking add button is clickable', async () => {
-//       const addBtn = await $('#add-to-cart-sauce-labs-bolt-t-shirt');
-//       await expect (addBtn).toBeClickable();
-//     });
-//   });
+    it('Checking add button is clickable', async () => {
+      const addBtn = await $('#add-to-cart-sauce-labs-bolt-t-shirt');
+      await expect (addBtn).toBeClickable();
+    });
+  });
 
-//   describe ('Jacket inventory test', () => {
+  describe ('Jacket inventory test', () => {
 
-//     it('Checking Img content', async () => {
-//       const imgSrc = await $('#item_5_img_link > img').getAttribute('src');
-//       await expect(imgSrc).toBe('/static/media/sauce-pullover-1200x1500.439fc934.jpg')
-//     });
+    it('Checking Img content', async () => {
+      const imgSrc = await $('#item_5_img_link > img').getAttribute('src');
+      await expect(imgSrc).toBe('/static/media/sauce-pullover-1200x1500.439fc934.jpg')
+    });
 
-//     it('Checking Img title link', async () => {
-//       const titleLink = await $('#item_5_title_link');
-//       await expect(titleLink).toHaveHrefContaining('#')
-//     });
+    it('Checking Img title link', async () => {
+      const titleLink = await $('#item_5_title_link');
+      await expect(titleLink).toHaveHrefContaining('#')
+    });
 
-//     it('Checking add button is clickable', async () => {
-//       const addBtn = await $('#add-to-cart-sauce-labs-fleece-jacket');
-//       await expect (addBtn).toBeClickable();
-//     });
-//   });
+    it('Checking add button is clickable', async () => {
+      const addBtn = await $('#add-to-cart-sauce-labs-fleece-jacket');
+      await expect (addBtn).toBeClickable();
+    });
+  });
 
-//   describe ('Onesie inventory test', () => {
+  describe ('Onesie inventory test', () => {
 
-//     it('Checking Img content', async () => {
-//       const imgSrc = await $('#item_2_img_link > img').getAttribute('src');
-//       await expect(imgSrc).toBe('/static/media/red-onesie-1200x1500.1b15e1fa.jpg')
-//     });
+    it('Checking Img content', async () => {
+      const imgSrc = await $('#item_2_img_link > img').getAttribute('src');
+      await expect(imgSrc).toBe('/static/media/red-onesie-1200x1500.1b15e1fa.jpg')
+    });
 
-//     it('Checking Img title link', async () => {
-//       const titleLink = await $('#item_2_title_link');
-//       await expect(titleLink).toHaveHrefContaining('#')
-//     });
+    it('Checking Img title link', async () => {
+      const titleLink = await $('#item_2_title_link');
+      await expect(titleLink).toHaveHrefContaining('#')
+    });
 
-//     it('Checking add button is clickable', async () => {
-//       const addBtn = await $('#add-to-cart-sauce-labs-onesie');
-//       await expect(addBtn).toBeClickable();
-//     });
-//   });
+    it('Checking add button is clickable', async () => {
+      const addBtn = await $('#add-to-cart-sauce-labs-onesie');
+      await expect(addBtn).toBeClickable();
+    });
+  });
 
-//   describe ('Red T-Shirt inventory test', () => {
+  describe ('Red T-Shirt inventory test', () => {
 
-//     it('Checking Img content', async () => {
-//       const imgSrc = await $('#item_3_img_link > img').getAttribute('src');
-//       await expect(imgSrc).toBe('/static/media/red-tatt-1200x1500.e32b4ef9.jpg')
-//     });
+    it('Checking Img content', async () => {
+      const imgSrc = await $('#item_3_img_link > img').getAttribute('src');
+      await expect(imgSrc).toBe('/static/media/red-tatt-1200x1500.e32b4ef9.jpg')
+    });
 
-//     it('Checking Img title link', async () => {
-//       const titleLink = await $('#item_3_title_link');
-//       await expect(titleLink).toHaveHrefContaining('#')
-//     });
-//   });
+    it('Checking Img title link', async () => {
+      const titleLink = await $('#item_3_title_link');
+      await expect(titleLink).toHaveHrefContaining('#')
+    });
+  });
 
-//   describe ('Inventory content', () => {
+  describe ('Inventory content', () => {
 
-//     it('Checking inventory description content', async () => {
-//       const inventoryDesc = await $('.inventory_item_label > div');
-//       await expect(inventoryDesc).toBeExisting();
-//     });
+    it('Checking inventory description content', async () => {
+      const inventoryDesc = await $('.inventory_item_label > div');
+      await expect(inventoryDesc).toBeExisting();
+    });
 
-//     it('Checking inventory price content', async () => {
-//       const titleLink = await $('#item_3_title_link');
-//       await expect(titleLink).toHaveHrefContaining('#')
-//     });
-//   });
+    it('Checking inventory price content', async () => {
+      const titleLink = await $('#item_3_title_link');
+      await expect(titleLink).toHaveHrefContaining('#')
+    });
+  });
 
-//   describe ('Header content', () => {
+  describe ('Header content', () => {
 
-//     it('Checking cart button to be existing', async () => {
-//       const cartIcon = await $('#shopping_cart_container');
-//       await expect(cartIcon).toBeExisting();
-//     });
+    it('Checking cart button to be existing', async () => {
+      const cartIcon = await $('#shopping_cart_container');
+      await expect(cartIcon).toBeExisting();
+    });
 
-//     it('Checking menu button to be clickable', async () => {
-//       const menuBtn = await $('#react-burger-menu-btn');
-//       await expect (menuBtn).toBeClickable();
-//     });
-//   });
+    it('Checking menu button to be clickable', async () => {
+      const menuBtn = await $('#react-burger-menu-btn');
+      await expect (menuBtn).toBeClickable();
+    });
+  });
 
-//   describe ('Span content', () => {
+  describe ('Span content', () => {
 
-//     it('Checking span image to be displayed', async () => {
-//       const spanImg = await $('.peek');
-//       await expect (spanImg).toBeDisplayed();
-//     });
-//   });
+    it('Checking span image to be displayed', async () => {
+      const spanImg = await $('.peek');
+      await expect (spanImg).toBeDisplayed();
+    });
+  });
 
-//   describe ('Social Media links', () => {
+  describe ('Social Media links', () => {
 
-//     it('Checking twitter link', async () => {
-//       const twitterLink = await $('.social_twitter a');
-//       await expect(twitterLink).toHaveLink('https://twitter.com/saucelabs');
-//     });
+    it('Checking twitter link', async () => {
+      const twitterLink = await $('.social_twitter a');
+      await expect(twitterLink).toHaveLink('https://twitter.com/saucelabs');
+    });
 
-//     it('Checking facebook link', async () => {
-//       const facebookLink = await $('.social_facebook a');
-//       await expect(facebookLink).toHaveLink('https://www.facebook.com/saucelabs');
-//     });
+    it('Checking facebook link', async () => {
+      const facebookLink = await $('.social_facebook a');
+      await expect(facebookLink).toHaveLink('https://www.facebook.com/saucelabs');
+    });
 
-//     it('Checking linkedin link', async () => {
-//       const linkedinLink = await $('.social_linkedin a');
-//       await expect(linkedinLink).toHaveLink('https://www.linkedin.com/company/sauce-labs/');
-//     });
-//   });
-// })
+    it('Checking linkedin link', async () => {
+      const linkedinLink = await $('.social_linkedin a');
+      await expect(linkedinLink).toHaveLink('https://www.linkedin.com/company/sauce-labs/');
+    });
+  });
+})
--- Desktop/Week-14/test/specs/login.spec.js
+++ Desktop/Week-14/test/specs/login.spec.js
@@ -1,82 +1,82 @@
-// const LoginPage = require('../pageobjects/login.page')
+const LoginPage = require('../pageobjects/login.page')
 
-// describe ('Login page testing', () => {
+describe ('Login page testing', () => {
 
-//   beforeAll('Open browser', () => {
-//     browser.url('https://www.saucedemo.com/')
-//   });
+  beforeAll('Open browser', () => {
+    browser.url('https://www.saucedemo.com/')
+  });
 
-//   describe('Username input test', () => {
+  describe('Username input test', () => {
 
-//     it('Empty username should display an error', async () => {
-//       await LoginPage.login('','secret_sauce');
-//       await expect(LoginPage.errorContainer).toHaveText('Epic sadface: Username is required');
-//     });
+    it('Empty username should display an error', async () => {
+      await LoginPage.login('','secret_sauce');
+      await expect(LoginPage.errorContainer).toHaveText('Epic sadface: Username is required');
+    });
 
-//     it('Invalid username', async () => {
-//       await LoginPage.login('invalid','secret_sauce');
-//       await expect(LoginPage.errorContainer).toHaveText('Epic sadface: Username and password do not match any user in this service');
-//     });
-//   });
+    it('Invalid username', async () => {
+      await LoginPage.login('invalid','secret_sauce');
+      await expect(LoginPage.errorContainer).toHaveText('Epic sadface: Username and password do not match any user in this service');
+    });
+  });
 
-//   describe ('Login with locked out user', () => {
+  describe ('Login with locked out user', () => {
 
-//     it('Using locked out user to log in should display an error', async () => {
-//       await LoginPage.open();
-//       await LoginPage.login('locked_out_user', 'secret_sauce');
-//       await expect(LoginPage.errorContainer).toHaveText('Epic sadface: Sorry, this user has been locked out.');
-//     });
-//   });
+    it('Using locked out user to log in should display an error', async () => {
+      await LoginPage.open();
+      await LoginPage.login('locked_out_user', 'secret_sauce');
+      await expect(LoginPage.errorContainer).toHaveText('Epic sadface: Sorry, this user has been locked out.');
+    });
+  });
 
-//   describe ('Login with problem user', () => {
+  describe ('Login with problem user', () => {
 
-//     it('Using problem user to log in', async () => {
-//       await LoginPage.open();
-//       await LoginPage.login('problem_user', 'secret_sauce');
-//       await expect(browser).toHaveUrl('https://www.saucedemo.com/inventory.html');
-//     });
+    it('Using problem user to log in', async () => {
+      await LoginPage.open();
+      await LoginPage.login('problem_user', 'secret_sauce');
+      await expect(browser).toHaveUrl('https://www.saucedemo.com/inventory.html');
+    });
 
-//     it('Checking images content when accessing with problem user', async () => {
-//       const imgSrc = await $('#item_4_img_link > img').getAttribute('src');
-//       await expect(imgSrc).toBe('/static/media/sl-404.168b1cce.jpg');
-//     });
-//   });
+    it('Checking images content when accessing with problem user', async () => {
+      const imgSrc = await $('#item_4_img_link > img').getAttribute('src');
+      await expect(imgSrc).toBe('/static/media/sl-404.168b1cce.jpg');
+    });
+  });
 
-//   describe ('Login with performance glitch user', () => {
+  describe ('Login with performance glitch user', () => {
 
-//     it ('Using performance glitch user to log in', async () => {
-//       await LoginPage.open();
-//       await LoginPage.login('performance_glitch_user', 'secret_sauce');
-//       await browser.setTimeout({'page load': 10000 });
-//       await expect(browser).toHaveUrl('https://www.saucedemo.com/inventory.html');
-//     });
-//   });
+    it ('Using performance glitch user to log in', async () => {
+      await LoginPage.open();
+      await LoginPage.login('performance_glitch_user', 'secret_sauce');
+      await browser.setTimeout({'page load': 10000 });
+      await expect(browser).toHaveUrl('https://www.saucedemo.com/inventory.html');
+    });
+  });
 
-//   describe('Login with valid user', () => {
+  describe('Login with valid user', () => {
 
-//     it('Login success', async () => {
-//       await LoginPage.open();
-//       await LoginPage.login('standard_user','secret_sauce');
-//       await expect(browser).toHaveUrl('https://www.saucedemo.com/inventory.html')
-//     });
-//   });
+    it('Login success', async () => {
+      await LoginPage.open();
+      await LoginPage.login('standard_user','secret_sauce');
+      await expect(browser).toHaveUrl('https://www.saucedemo.com/inventory.html')
+    });
+  });
 
-//   describe('Elements to be displayed', () => {
+  describe('Elements to be displayed', () => {
 
-//     it('Verify botImg to be displayed', async () =>{
-//       await LoginPage.open();
-//       await expect(LoginPage.botImg).toBeDisplayed();
-//     });
+    it('Verify botImg to be displayed', async () =>{
+      await LoginPage.open();
+      await expect(LoginPage.botImg).toBeDisplayed();
+    });
 
-//     it ('Verify login logo', async () => {
-//       await LoginPage.open();
-//       await expect(LoginPage.loginLogo).toBeDisplayed();
-//     });
+    it ('Verify login logo', async () => {
+      await LoginPage.open();
+      await expect(LoginPage.loginLogo).toBeDisplayed();
+    });
 
-//     it ('Img content', async () => {
-//       await LoginPage.login('standard_user','secret_sauce');
-//       const imgSrc = await $('#item_4_img_link > img').getAttribute('src');
-//       await expect(imgSrc).toBe('/static/media/sauce-backpack-1200x1500.34e7aa42.jpg')
-//     });
-//   });
-// });
+    it ('Img content', async () => {
+      await LoginPage.login('standard_user','secret_sauce');
+      const imgSrc = await $('#item_4_img_link > img').getAttribute('src');
+      await expect(imgSrc).toBe('/static/media/sauce-backpack-1200x1500.34e7aa42.jpg')
+    });
+  });
+});
